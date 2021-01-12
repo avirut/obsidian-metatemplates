47badcda95919c567cc3d74debca9504d00fe194
avirut <avirut.mehta@gmail.com>
2021-01-12 11:24:50 -0600

    fixed frontmatter parsing

--- main.ts
+++ main.ts
@@ -156,7 +156,7 @@ export default class Metatemplates extends Plugin {
   async fillTemplate(content: string) {
     let startInd = content.indexOf('---') + 4;
     let endInd = content.substring(startInd).indexOf('---') - 1;
-    let fmraw = content.substring(startInd, endInd);
+    let fmraw = content.substring(startInd, startInd + endInd);
     let fmparsed = jsyaml.load(fmraw);
 
     if (fmparsed['nameFormat']) {
